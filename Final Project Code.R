877e1a2117be466c34f967b465d6f594a95e7757
Pham <Daniel.Pham@stlcop.local>
2017-11-13 11:55:26 -0600

    Added descending dates
    Data organized by descending dates

--- Final Project Code.R
+++ Final Project Code.R
@@ -2,16 +2,20 @@
 library(tidyverse)
 library("dplyr")
 library(stringr)
-
+library(lubridate)
+library(plyr)
 
 acquisition_data <- read_csv('NADAC__National_Average_Drug_Acquisition_Cost_.csv') # Important to note that this data is from 2013 - 2017.
 
 print(acquisition_data) # Determining the drugs that are being analyzed.
 
+
+
 Adderall_data <- acquisition_data %>% # This data is going to separate Adderall from the other drugs to work with
   select(NDC_Description , Effective_Date, NADAC_Per_Unit, NDC)  %>%  #Select is used to choose the columns you want to work with
   filter(str_detect(NDC_Description, "ADDERALL|AMOXICILLIN|ALBUTEROL"))  # You can add several drugs in this str_detect with this | symbol
-  
+
+
 
 New_NDC <- str_pad(Adderall_data$NDC, 11 , pad = "0") #Makes all the strings 11 words long, not all manufaturer codes are 5 digits long
 
@@ -27,10 +31,22 @@ print(Adderall_data)
 #Albuterol_data_2.5 <- Adderall_data[Manufacturer_Code=="76204" & Product_Code=="0200" & Packaging_Code=="60"] # This line selects the exact Manufacturer_Code and the Product_Code 
 
 
-Albuterol_data_2.5 <- Adderall_data %>%                                        #Defined array for one particular drug.
-   filter(Manufacturer_Code == "76204", Product_Code == "0200", Packaging_Code == "60") #Filters the columns based on this criteria
+Albuterol_data_2.5 <- Adderall_data %>%                                        # Defined array for one particular drug.
+   filter(Manufacturer_Code == "76204", Product_Code == "0200", Packaging_Code == "60")  # Filters the columns based on this 
+
+# Albuterol_data_2.5[order(as.Date(Albuterol_data_2.5$Effective_Date , format="%d/%m/%Y")),,drop=FALSE]
+  
+Albuterol_data_2.5$Effective_Date <- mdy(Albuterol_data_2.5$Effective_Date)
+Albuterol_data_2.5 <- arrange(Albuterol_data_2.5 , desc(Effective_Date))
+
+
+
+#Albuterol_data_2.5 <- Albuterol_data_2.5[order(as.Date(Albuterol_data_2.5$Effective_Date,format="%d/%m/%Y")),,drop=FALSE]
+#Albuterol_data_2.5[order(as.Date(Albuterol_data_2.5$Effective_Date, format="%d/%m/%Y")),, drop=FALSE]
 
 print(Albuterol_data_2.5) 
+
+
 
 
 #///////////////////////////////////////////////////////////////////////THINGS TO DO//////////////////////////////////////////////////////////////////////////////////
@@ -55,7 +71,7 @@ print(Albuterol_data_2.5)
 # In addition to this, lets try plotting a line graph for this.
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------EXPERIMENTAL CODE FROM EARLIER----------------------------------------------------------------
+#---------------------------------------------------------------------EXPERIMENTAL CODE FROM EARLIER--------------------------------------------------------------
 # New_NDC <- sprintf("%012d",Adderall_data$NDC)
 # Sorted_NDC <- extract(Adderall_data, New_NDC, into = c("Product.Code", "Labeler.Code", "Packaging.Code"), regex = "(.{5})(.{4})(.{2})", remove = FALSE)
 # print(Sorted_NDC)
@@ -74,4 +90,4 @@ print(Albuterol_data_2.5)
 #Albuterol_data_2.5 <- Adderall_data[Manufacturer_Code=="76204" & Product_Code=="0200" & Packaging_Code=="60"] # This line selects the exact Manufacturer_Code and the Product_Code 
 
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------

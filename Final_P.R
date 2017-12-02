746c029bfb5161033f26cde5c8e9b4a879110b61
danp7 <daniel.pham@stlcop.edu>
2017-12-02 15:04:55 -0600

    Updated Final Version 2.0
    Added Graphs, minor bug fixes.

--- Final_P.R
+++ Final_P.R
@@ -7,12 +7,11 @@ library(plyr)
 library(scales)
 library(ggplot2)
 
-acquisition_data <- read_csv('NADAC__National_Average_Drug_Acquisition_Cost_.csv') # Important to note that this data is from 2013 - 2017.
-
-
-print(acquisition_data) # Determining the drugs that are being analyzed.
-
 # This is the final version 11/29/2017
+# Added Graphs, minor changes to user interface.
+
+
+acquisition_data <- read_csv('NADAC__National_Average_Drug_Acquisition_Cost_.csv') # Important to note that this data is from 2013 - 2017.
 
 Sorted_data <- acquisition_data %>% # This data is going to separate Adderall from the other drugs to work with
   select(NDC_Description , Effective_Date, NADAC_Per_Unit, NDC) #  %>%  #Select is used to choose the columns you want to work with
@@ -27,9 +26,7 @@ Product_Code <- substr(New_NDC, 6, 9) # Product Code, middle 4 digits
 Packaging_Code <- substr(New_NDC, 10, 11) # Packaging Code, each drug has an individual packaging code
 
 Sorted_data <- cbind(Sorted_data , New_NDC , Manufacturer_Code, Product_Code, Packaging_Code) # Combines the two lines of code together.
-
-
-#Albuterol_data_2.5 <- Sorted_data[Manufacturer_Code=="76204" & Product_Code=="0200" & Packaging_Code=="60"] # This line selects the exact Manufacturer_Code and the Product_Code 
+View(Sorted_data)
 
 
 Albuterol_data_2.5 <- Sorted_data %>%                                        # Defined array for one particular drug.
@@ -44,13 +41,20 @@ PenVK <- Sorted_data %>%
 Amox <- Sorted_data %>%
   filter(Manufacturer_Code == "00781", Product_Code == "2613")
 
+# Added IBUPROFEN
+Ibu <- Sorted_data %>%
+  filter(Manufacturer_Code == "53746", Product_Code == "0140" , Packaging_Code == "10")
 
-# Albuterol_data_2.5[order(as.Date(Albuterol_data_2.5$Effective_Date , format="%d/%m/%Y")),,drop=FALSE]
+Ibu <- Sorted_data %>%
+  filter(Manufacturer_Code == "53746", Product_Code == "0140" , Packaging_Code == "10")
+
+Predn <- Sorted_data %>%
+  filter(Manufacturer_Code == "13925", Product_Code == "0501" , Packaging_Code == "04")
 
 Albuterol_data_2.5$Effective_Date <- mdy(Albuterol_data_2.5$Effective_Date)
 Albuterol_data_2.5 <- arrange(Albuterol_data_2.5 , (Effective_Date))
 
-Adderall$Effective_Date <- mdy(Adderall$Effective_Date)
+Adderall$Effective_Date <- mdy(Adderall$Effective_Date) # The lines here sort the dates so they can be graphed.
 Adderall <- arrange(Adderall , (Effective_Date))
 
 PenVK$Effective_Date <- mdy(PenVK$Effective_Date)
@@ -59,17 +63,23 @@ PenVK <- arrange(PenVK , (Effective_Date))
 Amox$Effective_Date <- mdy(Amox$Effective_Date)
 Amox <- arrange(Amox , (Effective_Date))
 
+Ibu$Effective_Date <- mdy(Ibu$Effective_Date)
+Ibu <- arrange(Ibu , (Effective_Date))
 
-#Albuterol_data_2.5 <- Albuterol_data_2.5[order(as.Date(Albuterol_data_2.5$Effective_Date,format="%d/%m/%Y")),,drop=FALSE]
-#Albuterol_data_2.5[order(as.Date(Albuterol_data_2.5$Effective_Date, format="%d/%m/%Y")),, drop=FALSE]
+Predn$Effective_Date <- mdy(Predn$Effective_Date)
+Predn <- arrange(Predn , (Effective_Date))
 
-print(Albuterol_data_2.5) 
+head(Albuterol_data_2.5) 
 
-print(Adderall)
+head(Adderall)
 
-print(PenVK)
+head(PenVK)
 
-print(Amox)
+head(Amox)
+
+head(Ibu)
+
+head(Predn)
 
 #These are the graphs you're looking for!
 
@@ -93,6 +103,16 @@ ggplot(Amox, aes(Effective_Date, NADAC_Per_Unit, size = 2)) + geom_line(color = 
   xlab("Effective Date (Year-Month)") + ylab("NADAC Per Unit (Dollars Each)") + 
   labs(title = "Drug Unit Prices", subtitle = "Amoxicillin 500mg")
 
+ggplot(Ibu, aes(Effective_Date, NADAC_Per_Unit, size = 2)) + geom_line(color = "purple") +
+  scale_x_date(date_labels = "%Y-%m", expand = c(0,50), breaks = date_breaks("3 months")) +
+  xlab("Effective Date (Year-Month)") + ylab("NADAC Per Unit (Dollars Each)") + 
+  labs(title = "Drug Unit Prices", subtitle = "Ibuprofen 200mg")
+
+ggplot(Predn, aes(Effective_Date, NADAC_Per_Unit, size = 2)) + geom_line(color = "gray") +
+  scale_x_date(date_labels = "%Y-%m", expand = c(0,50), breaks = date_breaks("3 months")) +
+  xlab("Effective Date (Year-Month)") + ylab("NADAC Per Unit (Dollars Each)") + 
+  labs(title = "Drug Unit Prices", subtitle = "Prednisolone 5mg/5ml Soln")
+
 # ggplot(Albuterol_data_2.5, aes(Effective_Date, NADAC_Per_Unit)) + geom_line() +
 #  scale_x_date(format = "%b-%Y") + xlab("") + ylab("NADAC Per Unit")
 
@@ -113,19 +133,19 @@ Medicaidinput <- function()
   
   print(paste("The NDC code that you have inputed is", m, "-", p, "-" , pa))
   
-  if (m %in% Sorted_data$Manufacturer_Code && p %in% Sorted_data$Product_Code && pa %in% Sorted_data$Packaging_Code){
-    print("Alright, we have found the drug that you were searching for.") # find out what %in% is.
+  if (m %in% Sorted_data$Manufacturer_Code && p %in% Sorted_data$Product_Code && pa %in% Sorted_data$Packaging_Code){ #Finds out if these values
+    print("Alright, we have found the drug that you were searching for.") # find out what %in% is.      #are in the data base or not.
     
     UserNDC <- Sorted_data %>%
-      filter(Manufacturer_Code == m, Product_Code == p, Packaging_Code == pa)
+      filter(Manufacturer_Code == m, Product_Code == p, Packaging_Code == pa) # Matches user input with row values.
     
-    UserNDC$Effective_Date <- mdy(UserNDC$Effective_Date)
+    UserNDC$Effective_Date <- mdy(UserNDC$Effective_Date) #These two lines organize dates so they can be graphed
     UserNDC <- arrange(UserNDC , (Effective_Date))
     usertitle <- unique(UserNDC$NDC_Description) # Unique identifies unique variables in vector, usertitle would not work if there 
     # More than one unique value.
     
     View(UserNDC)
-    
+    head(UserNDC)
     
     graph <- ggplot(UserNDC, aes(Effective_Date, NADAC_Per_Unit, size = 2)) + geom_line(color = "yellow") +
       scale_x_date(date_labels = "%Y-%m", expand = c(0,10), breaks = date_breaks("3 months")) +
@@ -137,10 +157,8 @@ Medicaidinput <- function()
     print("Unfortunately, we could not find the drug that you are looking for in this database.")
   }
   
-  
-  return(m)
+
 }
 
 print(Medicaidinput())
-head(Sorted_data)
 
